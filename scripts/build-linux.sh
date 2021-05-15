a670b415105289d3445cc4219d5878c50af2d973
GongT <admin@gongt.me>
2021-05-16 07:38:43 +0800

    update comment

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -10,4 +10,4 @@ go build -o "$D/client" cmd/client.go
 
 # scp "$D/client" server:/usr/local/bin/remote-run
 # cp "$D/client" /usr/local/bin/remote-run
-# podman cp /data/DevelopmentRoot/github.com/gongt/remote-shell/dist/linux/client qbittorrent:/usr/bin/x-www-browser
+# podman cp /data/DevelopmentRoot/github.com/gongt/remote-shell/dist/linux/client qbittorrent:/bin/x-www-browser
