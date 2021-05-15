c36b831136e788897bc5a9cadf6c789943beecb2
GongT <admin@gongt.me>
2021-05-16 04:23:00 +0800

    fix wrong ip usage

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -2,7 +2,8 @@ package constants
 
 import "time"
 
-const MulticastAddress = "224.6.0.0:23333"
+// const MulticastAddress = "[ff12::feed:a:dead:beef]:2333"
+const MulticastAddress = "224.1.2.3:23333"
 const Timeout = 3 * time.Second
 
 const FileOpenLocalPrefix = "/data/Volumes/"
